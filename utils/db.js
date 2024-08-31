b7db751ecf1395a28acf737c72d8057533d361c5
michael-shambel <Michaelshambel10@gmail.com>
2024-08-31 21:50:02 +0300

    task 1 restore

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,12 @@
 import { MongoClient } from 'mongodb';
 
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || '27017';
+const DB_DATABASE = process.env.DB_DATABASE || 'file_manager';
+
 class DBClient {
   constructor() {
-    const DB_HOST = process.env.DB_HOST || 'localhost';
-    const DB_PORT = process.env.DB_PORT || '27017';
-    const DB_DATABASE = process.env.DB_DATABASE || 'file_manager';
     this.url = `mongodb://${DB_HOST}:${DB_PORT}`;
     this.dbName = DB_DATABASE;
     this.client = new MongoClient(this.url, { useNewUrlParser: true, useUnifiedTopology: true });
