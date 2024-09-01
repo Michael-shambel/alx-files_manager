08d6d0b1441a8702d582be8044b325a5b8bd85ae
michael-shambel <Michaelshambel10@gmail.com>
2024-09-02 01:57:15 +0300

    task 1

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.dbName = database;
+
+    this.client.connect()
+        .then(() => {
+            this.db = this.client.db(this.dbName);
+        })
+        .catch((err) => {
+            console.error('Error connecting to MongoDB:', err);
+        });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    const count = await usersCollection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const filesCollection = this.db.collection('files');
+    const count = await filesCollection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
