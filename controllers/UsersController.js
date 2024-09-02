bada0a3332143175be6ec7e10c435aa94089b4c8
michael-shambel <Michaelshambel10@gmail.com>
2024-09-02 18:40:28 +0300

    task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -24,6 +25,28 @@ class UsersController {
     const result = await dbClient.db.collection('users').insertOne({ email, password: passwordHash });
 
     return res.status(201).json({ email, password, id: result.insertedId });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['s-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: dbClient.getObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
