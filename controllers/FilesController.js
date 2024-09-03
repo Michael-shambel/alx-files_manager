5cdaeb852779ea0c77919e240c1feddffebcf2cd
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-09-03 07:07:52 +0300

    Get and list file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -75,6 +75,76 @@ class FilesController {
     await dbClient.db.collection('files').insertOne(fileDocument);
     return res.status(201).json(fileDocument);
   }
+
+  static async getShow(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const fileId = req.params.id;
+    const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    return res.status(200).json(file);
+  }
+
+  static async getIndex(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const parentId = req.query.parentId || '0';
+    const page = parseInt(req.query.page, 10) || 0;
+
+    const filesQuery = {
+      userId: ObjectId(userId),
+    };
+
+    if (parentId !== '0') {
+      filesQuery.parentId = ObjectId(parentId);
+    } else {
+      filesQuery.parentId = '0';
+    }
+
+    const files = await dbClient.db.collection('files')
+      .find(filesQuery)
+      .skip(page * 20)
+      .limit(20)
+      .toArray();
+
+    return res.status(200).json(files);
+  }
 }
 
 export default FilesController;
